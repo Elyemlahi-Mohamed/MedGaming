b47588a848759b035e2809a83acde25f9a3ff18d
Mohamed <122557510+Elyemlahi-Mohamed@users.noreply.github.com>
2023-06-12 09:27:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,15 +4,46 @@ let landing = document.querySelector('.landing');
 let closeMega = document.querySelector('.close');
 
 megaMenu.classList.add('hide');
-otherLinks.onclick = function () {
+otherLinks.onclick = () => {
     megaMenu.classList.remove('hide');
 }
 
-closeMega.onclick = function () {
+closeMega.onclick = () => {
     megaMenu.classList.add('hide');
-    
+
 }
 
-landing.onclick = function () {
+landing.onclick = () => {
     megaMenu.classList.add('hide');
+};
+
+
+let btn = document.getElementById('btn');
+
+
+window.onscroll = () => {
+    if (scrollY >= 400) {
+        btn.style.display = 'block'
+    } else {
+        btn.style.display = 'none'
+    }
 }
+
+btn.onclick = () => {
+    window.scroll({
+        left: 0,
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+let scroll = document.querySelector(".scroller");
+let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+
+window.addEventListener("scroll", () => {
+    let = scrollTop = document.documentElement.scrollTop;
+    scroll.style.width = `${(scrollTop / height) * 100}%`
+});
+
+
